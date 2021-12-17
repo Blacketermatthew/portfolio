2eefe46a415de85f5327c163b06ae16c7add3539
Matthew Blacketer <matthew.blacketer@epicor.com>
2021-12-17 14:16:47 -0600

    working on adding color to the active nav

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,8 @@
 // Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+var header = document.getElementById("navbar-list");
+var navs = header.getElementsByClassName("li");
+for (var i = 0; i < navs.length; i++) {
+  navs[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(" active", "");
   this.className += " active";
