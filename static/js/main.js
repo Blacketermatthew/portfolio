980edd7f4f446aee582b641e4a4c610fa1c1dc51
Matthew Blacketer <matthew.blacketer@epicor.com>
2022-01-18 23:26:20 -0600

    moved js to bottom; nav now auto-closes

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,18 @@
 // Add active class to the current button (highlight it)
-var header = document.getElementById("navbar-list");
-var navs = header.getElementsByClassName("li");
-for (var i = 0; i < navs.length; i++) {
-  navs[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+
+// var header = document.getElementById("navbar-list");
+// var navs = header.getElementsByClassName("li");
+// for (var i = 0; i < navs.length; i++) {
+//   navs[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace(" active", "");
+//   this.className += " active";
+//   });
+// }
+
+const navLinks = document.querySelectorAll('.nav-link')
+const menuToggle = document.getElementById('navbarDropdown')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
