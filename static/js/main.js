bf4e1f529c30ceeb9e278d07907ee616982dcedb
Matthew Blacketer <matthew.blacketer@epicor.com>
2022-01-18 23:57:15 -0600

    started working on active navbar js stuff

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,13 @@
 // Add active class to the current button (highlight it)
 
 // var header = document.getElementById("navbar-list");
-// var navs = header.getElementsByClassName("li");
-// for (var i = 0; i < navs.length; i++) {
-//   navs[i].addEventListener("click", function() {
-//   var current = document.getElementsByClassName("active");
-//   current[0].className = current[0].className.replace(" active", "");
-//   this.className += " active";
-//   });
-// }
+// var navs = header.querySelectorAll("nav-link");
+// navs.forEach((l) => {
+//   l.addEventListener("click", () => {
+//     l[0].className = current[0].
+//     this.className += " active";
+//   })
+// })
 
 const navLinks = document.querySelectorAll('.nav-link')
 const menuToggle = document.getElementById('navbarDropdown')
