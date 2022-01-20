191f59201f79f8f587edf02cae2cae28d7fe38b7
Blacketermatthew <blacketermatthew@gmail.com>
2022-01-19 21:59:49 -0600

    Tidied up html/css, started on recaptcha

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,18 @@
-// Add active class to the current button (highlight it)
-var header = document.getElementById("navbar-list");
-var navs = header.getElementsByClassName("li");
-for (var i = 0; i < navs.length; i++) {
-  navs[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+// For closing the collapsed navbar after an option is selected
+// const navLinks = document.querySelectorAll('.nav-link')
+// const menuToggle = document.getElementById('navbarNavDarkDropdown')
+// const bsCollapse = new bootstrap.Collapse(menuToggle)
+
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { bsCollapse.toggle() })
+// })
+
+
+
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
